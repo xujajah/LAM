b37a586bff0d45eed19fd819e49ad4902e359012
zujajah <xujajah@gmail.com>
2020-08-20 21:49:13 +0500

    Initial commit

---
+++ js/language.js
@@ -0,0 +1,167 @@
+let lang = {
+    en: {
+        "homepage": "Home",
+        "services": "Services",
+        "prices": "Prices",
+        "masseuses": "Masseuses",
+        "booking": "Booking",
+        "RU": "RU",
+        "aboutMessage": "About Massage",
+        "aboutParagraph": "One of the oldest healing techniques. A natural remedy against pain, it was and is an instrument of communication. Where knowledge and experience are transmitted through direct contact. This knowledge has passed from generation to generation and today the massages have specialized according to the needs, to obtain relief, relaxation or prevent injuries and even diseases",
+        "ourTeam": "Our team",
+        "ourTeamParagraph": "We are a group of accredited masseurs who continually specialize in new techniques or methods and not only within the area of massage but also in areas like fitness or nutrition. Among us we share the knowledge and experience to achieve the best treatment and service for our clients and to be ready for any situation.",
+        "beforeMassage": "Before Massage",
+        "beforeMassageParagraph": "You should consider some contraindications. For example, inflammations, fever, allergies, skin diseases, burns, wounds with bleeding, period of menstruation or cases such as cancer. It is important to inform in case of pregnancy. At the time of the massage, remember that it is a direct contact treatment where you should inform the therapist if you have an uncomfortable situation.",
+        "classic": "Classic Massage",
+        "classicParagraph": "This massage helps you to relax your entire body from feet to neck. where your muscles will tonify, and your blood circulation will improve therefore you will feel lighter. In this procedure will not feel heavy pressure.",
+        "reconditioning": "Reconditioning Massage",
+        "reconditioningParagraph": "This massage is recommended for people who carry an average physical activity whether they carry out routine work or practice sports without any training. Mobility problems of the body are present as well fatigue. It works focused on muscles and joints",
+        "deepTissue": "Deep Tissue Massage",
+        "deepTissueParagraph": "This massage is not for the whole body and is not long lasting, it focuses on the problematic parts with recurrence. It can cause pain during the procedure, since it is necessary to use a deep manipulation to achieve stretching of tissues such as ligament, insertions or facies.",
+        "sports": "Sports Massage",
+        "sportsParagraph": "This massage gives more emphasis on preparing the muscles, ligaments and joints for a physical activity that requires effort. It is also indicated for people returning from a long period of rest or to prevent injuries before a competition..",
+        "relaxing": "Relaxing Massage",
+        "relaxingParagraph": "We use the bases of a classic massage for the whole body with the difference that the pressure is softer and synchronized. We use more oil and we can even add a little aroma to get faster to a point of tranquility and peace. It is recommended for people who want to disconnect the mind from the tasks of the day. You can ask your therapist for other relaxation massage techniques like Lomi Lomi or Aroma massage",
+        "parental": "Parental Massage",
+        "parentalParagraph": "It is very important to keep the muscles and the whole body in a good balance even during the period of pregnancy. Our therapists have the knowledge and experience necessary to provide care and relaxation during this important period.",
+        "reservation": "MAKE A RESERVATION NOW",
+        "time": "Time",
+        "price": "Price",
+        "priceText": "* These prices are for guidance only, each procedure varies in price according to the difficulty or the qualified knowledge.",
+        "qualifiedMasseuses": "Our Qualified Masseuses",
+        "knowAboutHim": "Know more about him",
+        "jorge": "Jorge",
+        "george": "George",
+        "maria": "Maria",
+        "aboutMe": "About Me",
+        "aboutJorge": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "rotateBack": "Rotate Back",
+        "aboutGeorge": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "aboutMaria": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "copyright": "2020 Copyright:",
+        "laMassage": "LA Massage"
+    },
+    ru: {
+        "homepage": "Домой",
+        "services": "Сервисы",
+        "prices": "Цены",
+        "masseuses": "Массажистки",
+        "booking": "бронирование",
+        "lang": "RU",
+        "aboutMessage": "О массаже ",
+        "aboutParagraph": "Одна из древнейших лечебных техник. Естественное средство от боли, он был и остается инструментом общения. Где знания и опыт передаются через прямой контакт. Эти знания передавались из поколения в поколение, и сегодня массажи специализируются в соответствии с потребностями, чтобы получить облегчение, расслабление или предотвратить травмы и даже болезни.",
+        "ourTeam": "Наша команда",
+        "ourTeamParagraph": "Мы - группа аккредитованных массажистов, которые постоянно специализируются на новых техниках и методах, причем не только в области массажа, но и в таких областях, как фитнес или питание. Мы делимся своими знаниями и опытом, чтобы обеспечить наилучшее лечение и обслуживание наших клиентов и быть готовыми к любой ситуации.",
+        "beforeMassage": "Перед массажем",
+        "beforeMassageParagraph": "Следует учитывать некоторые противопоказания. Например, воспаления, лихорадка, аллергия, кожные заболевания, ожоги, раны с кровотечением, период менструации или такие случаи, как рак. Об этом важно сообщить в случае беременности. Во время массажа помните, что это процедура прямого контакта, при которой вы должны сообщить терапевту, если у вас возникнет неудобная ситуация.",
+        "classic": "Классический массаж",
+        "classicParagraph": "Этот массаж помогает расслабить все тело от ступней до шеи. где ваши мышцы будут тонизированы, а кровообращение улучшится, поэтому вы почувствуете себя легче. В этой процедуре не будет ощущаться сильного давления.",
+        "reconditioning": "Восстанавливающий массаж ",
+        "reconditioningParagraph": "Этот массаж рекомендуется людям со средней физической активностью, будь то повседневная работа или занятия спортом без какой-либо подготовки. Присутствуют проблемы с подвижностью тела, а также переутомление. Он работает с упором на мышцы и суставы",
+        "deepTissue": "Массаж глубоких тканей",
+        "deepTissueParagraph": "Этот массаж не для всего тела и недолговечный, он периодически фокусируется на проблемных частях. Это может вызвать боль во время процедуры, так как необходимо использовать глубокую манипуляцию, чтобы добиться растяжения тканей, таких как связки, вставки или лица.",
+        "sports": "Спортивный массаж ",
+        "sportsParagraph": "Этот массаж уделяет больше внимания подготовке мышц, связок и суставов к физической активности, требующей усилий. Он также показан людям, возвращающимся из длительного периода отдыха, или для предотвращения травм перед соревнованиями.",
+        "relaxing": "Расслабляющий массаж",
+        "relaxingParagraph": "Мы используем основы классического массажа для всего тела с той разницей, что давление более мягкое и синхронизированное. Мы используем больше масла и даже можем добавить немного аромата, чтобы быстрее достичь состояния спокойствия и умиротворения. Рекомендуется людям, которые хотят отвлечься от повседневных дел. Вы можете спросить своего терапевта о других методах расслабляющего массажа, таких как Ломи Ломи или Арома массаж.",
+        "parental": "Родительский массаж ",
+        "parentalParagraph": "Очень важно поддерживать баланс мышц и всего тела даже во время беременности. Наши терапевты обладают знаниями и опытом, необходимыми для ухода и расслабления в этот важный период.",
+        "reservation": "ЗАБРОНИРОВАТЬ СЕЙЧАС ",
+        "time": "Время",
+        "price": "Цена",
+        "priceText": "* Эти цены являются ориентировочными, каждая процедура различается по цене в зависимости от сложности или квалификационных знаний",
+        "qualifiedMasseuses": "Наши массажистки",
+        "knowAboutHim": "Узнать о нем больше",
+        "jorge": "Jorge",
+        "george": "George",
+        "maria": "Maria",
+        "aboutMe": "Обо мне",
+        "aboutJorge": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "rotateBack": "Повернуть назад",
+        "aboutGeorge": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "aboutMaria": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quae, dolores dicta. Blanditiis rem amet repellat, dolores nihil quae in mollitia asperiores ut rerum repellendus, voluptatum eum, officia laudantium quaerat?",
+        "copyright": "2020 Авторские права:",
+        "laMassage": "LA Massage"
+    },
+}
+
+$(document).ready(() => {
+    $('#btn-en').hide()
+    $('#btn-ru').show()
+})
+
+function switchLang(language) {
+    if (language === 'ru') {
+        $('#btn-en').show()
+        $('#btn-ru').hide()
+    } else if (language === 'en') {
+        $('#btn-en').hide()
+        $('#btn-ru').show()
+    }
+    $('#nav-home').text(lang[language].homepage);
+    $('#nav-booking').text(lang[language].booking);
+    $('#nav-masseuses').text(lang[language].masseuses);
+    $('#nav-price').text(lang[language].prices);
+    $('#nav-service').text(lang[language].services);
+
+    $('#h-about').text(lang[language].aboutMessage);
+    $('#p-about').text(lang[language].aboutParagraph);
+
+    $('#h-team').text(lang[language].ourTeam);
+    $('#p-team').text(lang[language].ourTeamParagraph);
+
+    $('#h-beforeMassage').text(lang[language].beforeMassage);
+    $('#p-beforeMassage').text(lang[language].beforeMassageParagraph);
+
+    $('#h-service').text(lang[language].services);
+
+    $('#h-classic').text(lang[language].classic);
+    $('#p-classic').text(lang[language].classicParagraph);
+
+    $('#h-deep-tissue').text(lang[language].deepTissue);
+    $('#p-deep-tissue').text(lang[language].deepTissueParagraph);
+
+    $('#h-sports').text(lang[language].sports);
+    $('#p-sports').text(lang[language].sportsParagraph);
+
+    $('#h-relaxing').text(lang[language].relaxing);
+    $('#p-relaxing').text(lang[language].relaxingParagraph);
+
+    $('#h-prenatal').text(lang[language].parental);
+    $('#p-prenatal').text(lang[language].parentalParagraph);
+
+    $('#h-recondition').text(lang[language].reconditioning);
+    $('#p-recondition').text(lang[language].reconditioningParagraph);
+
+    $('#btn-reservation').text(lang[language].reservation);
+
+    $('#h-prices').text(lang[language].prices);
+
+    $('#table-price').text(lang[language].price);
+    $('#table-time').text(lang[language].time);
+
+    $('#p-price-text').text(lang[language].priceText);
+
+    $('#h-qualified-team').text(lang[language].qualifiedMasseuses);
+
+    $('#p-know-1').text(lang[language].knowAboutHim);
+    $('#p-know-2').text(lang[language].knowAboutHim);
+    $('#p-know-3').text(lang[language].knowAboutHim);
+
+    $('#h-about-1').text(lang[language].aboutMe);
+    $('#h-about-2').text(lang[language].aboutMe);
+    $('#h-about-3').text(lang[language].aboutMe);
+
+    $('#btn-rotate-1').text(lang[language].rotateBack);
+    $('#btn-rotate-2').text(lang[language].rotateBack);
+    $('#btn-rotate-3').text(lang[language].rotateBack);
+
+    $('#p-about-jorge').text(lang[language].aboutJorge);
+    $('#p-about-george').text(lang[language].aboutGeorge);
+    $('#p-about-maria').text(lang[language].aboutMaria);
+
+    $('#copyright').text(lang[language].copyright);
+    $('#laMassage').text(lang[language].laMassage);
+
+
+}
